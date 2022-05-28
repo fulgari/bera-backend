b9b16b92ac6e48d9170fc63672672ad9bad832c5
Johnny Pan <swanf@outlook.com>
2022-05-28 11:36:24 +0800

    chore: Add express

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('./express/app');
+const PORT = 9001;
+
+function init() {
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  })
+}
+
+init();
