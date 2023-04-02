b16ebe6437cbe317b89e82a75b4efd55566769d8
Johnny Pan <swanf@outlook.com>
2023-04-02 23:56:00 +0800

    feat: docker-compose, mysql & backend

--- README.md
+++ README.md
@@ -23,9 +23,33 @@ Backend service for Bera App, a Kanban-like daily task management app for develo
 
 # Dockerize
 
-## build
+## DB
+
+Dump the mysql table struct into a `.sql` file in order to create copies of tables in the docker containers.
+
+```
+mysqldump -h <your_mysql_host> -u <user_name> -p --no-data <schema_name> > schema.sql
+```
+For our specific case:
+```
+mysqldump testdb -h 127.0.0.1 -u root -p --no-data > schema.sql
+```
+
+## Bera-backend
 
 We have the build argument `DOCKER_ENV` which will be used in the `Dockerfile` commands as variable. `-t` tags the images with a name of `bera-backend` for clearance. And the root directory is `.`, the current working directory.
 ```bash
 docker build --build-arg DOCKER_ENV=production -t bera-backend .
+```
+
+## execute
+
+Once we have set up, we can run the following commands to serve the DB and the backend service.
+
+```
+docker-compose build
+```
+
+```
+docker-compose up
 ```
---
+++ src/database/dump/schema.sql
@@ -0,0 +1,91 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for macos13.0 (arm64)
+--
+-- Host: 127.0.0.1    Database: testdb
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `kanbans`
+--
+
+DROP TABLE IF EXISTS `kanbans`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `kanbans` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `userId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`),
+  CONSTRAINT `kanbans_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `todorecords`
+--
+
+DROP TABLE IF EXISTS `todorecords`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `todorecords` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` varchar(255) NOT NULL,
+  `listId` varchar(255) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `text` varchar(255) NOT NULL,
+  `done` tinyint(1) DEFAULT NULL,
+  `modifiedAt` varchar(255) NOT NULL,
+  `createdAt` varchar(255) NOT NULL,
+  `isMD` tinyint(1) DEFAULT NULL,
+  `tags` varchar(255) DEFAULT NULL,
+  `updatedAt` datetime NOT NULL,
+  `kanbanId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `kanbanId` (`kanbanId`),
+  CONSTRAINT `todorecords_ibfk_1` FOREIGN KEY (`kanbanId`) REFERENCES `kanbans` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-02 23:29:30
