1a92d7338a38bb70db7489af6dee20bf31c44a3b
Johnny Pan <swanf@outlook.com>
2023-05-27 14:06:48 +0800

    feat: vercel compliance WIP

---
+++ api/index.js
@@ -0,0 +1,3 @@
+const app = require('../src/express/app');
+
+module.exports = app;
--- src/express/app.js
+++ src/express/app.js
@@ -32,6 +32,8 @@ app.use(bodyParser.urlencoded());
 function makeHandlerAwareOfAsyncErrors(handler) {
   return async function (req, res, next) {
     try {
+      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
       await handler(req, res);
     } catch (error) {
       next(error);
