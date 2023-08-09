5e21592c35a4d0eb3a028ab9873fb38bc2c96f16
Johnny <swanf@outlook.com>
2023-08-10 00:30:39 +0800

    feat: jwt added

--- src/auth/controllers/userController.js
+++ src/auth/controllers/userController.js
@@ -3,31 +3,33 @@ var mongoose = require('mongoose'),
   jwt = require('jsonwebtoken'),
   bcrypt = require('bcrypt'),
   User = mongoose.model('User');
+
 exports.register = function (req, res) {
   var newUser = new User(req.body);
   newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
-  newUser.save(function (err, user) {
-    if (err) {
-      return res.status(400).send({
-        message: err
-      });
-    } else {
-      user.hash_password = undefined;
-      return res.json(user);
-    }
+  newUser.save().then(user => {
+    user.hash_password = undefined;
+    return res.json(user);
+  }).catch((err) => {
+    return res.status(400).send({
+      message: err
+    });
   });
 };
+
 exports.sign_in = function (req, res) {
   User.findOne({
     email: req.body.email
-  }, function (err, user) {
-    if (err) throw err;
+  }).then(user => {
     if (!user || !user.comparePassword(req.body.password)) {
       return res.status(401).json({ message: 'Authentication failed. Invalid user or password.' });
     }
     return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, 'RESTFULAPIs') });
+  }).catch(err => {
+    throw err;
   });
 };
+
 exports.loginRequired = function (req, res, next) {
   if (req.user) {
     next();
@@ -35,6 +37,7 @@ exports.loginRequired = function (req, res, next) {
     return res.status(401).json({ message: 'Unauthorized user!!' });
   }
 };
+
 exports.profile = function (req, res, next) {
   if (req.user) {
     res.send(req.user);
---
+++ src/auth/server.js
@@ -0,0 +1,74 @@
+'use strict';
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  User = require('./models/userModel'),
+  bodyParser = require('body-parser'),
+  jsonwebtoken = require("jsonwebtoken"),
+  env = require('dotenv').config().parsed;
+const mongoose = require('mongoose');
+const option = {
+    socketTimeoutMS: 30000,
+    // keepAlive: true,
+    // reconnectTries: 30000
+};
+const mongoURI = process.env.MONGODB_URI;
+const uri = `mongodb+srv://swanf:${mongoURI || env.AUTH_DB_PASSWORD}@bera-auth-db.bedrsfr.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(uri, option).then(function(){
+    //connected successfully
+    console.log("Connect to mongo successfully")
+}, function(err) {
+    //err handle
+    console.log("Can not connect to mongo!", err)
+});
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
+      if (err) req.user = undefined;
+      req.user = decode;
+      next();
+    });
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
+var routes = require('./route/userRoute');
+routes(app);
+app.use(function(req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found' })
+});
+app.listen(port);
+console.log(' RESTful API server started on: ' + port);
+module.exports = app;
