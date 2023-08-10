b97ed869e33a12e5115b6e980a9460868c781b0d
Johnny <swanf@outlook.com>
2023-08-10 13:14:35 +0800

    chore: cleanup

--- src/auth/server.js
+++ src/auth/server.js
@@ -7,8 +7,6 @@ var express = require('express'),
 const mongoose = require('mongoose');
 const option = {
     socketTimeoutMS: 30000,
-    // keepAlive: true,
-    // reconnectTries: 30000
 };
 const mongoURI = process.env.MONGODB_URI;
 const uri = mongoURI || `mongodb+srv://${env.AUTH_DB_USERNAME}:${env.AUTH_DB_PASSWORD}@bera-auth-db.bedrsfr.mongodb.net/?retryWrites=true&w=majority`;
@@ -19,34 +17,6 @@ mongoose.connect(uri, option).then(function(){
     //err handle
     console.log("Can not connect to mongo!", err)
 });
-
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -67,6 +37,5 @@ routes(app);
 app.use(function(req, res) {
   res.status(404).send({ url: req.originalUrl + ' not found' })
 });
-// app.listen(port);
-// console.log('Auth RESTful API server started on: ' + port);
+
 module.exports = app;
